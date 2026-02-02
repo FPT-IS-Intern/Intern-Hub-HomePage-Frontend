aa56210a6915cc3fb0838783d4ad0f01b78d4375
vominhdat68 <datminhvo0901@gmail.com>
2026-02-02 14:02:08 +0700

    update api weekday contrainer

--- src/app/features/weekly-attendance/components/weekday-selector/weekday-container.component.ts
+++ src/app/features/weekly-attendance/components/weekday-selector/weekday-container.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { WeekdaySelectorComponent } from './weekday-selector.component'; // Đảm bảo đúng đường dẫn
+import { Component, OnInit, inject } from '@angular/core';
+import { WeekdaySelectorComponent } from './weekday-selector.component';
+import { Weekday } from '../../models/weekday.model';
+import { ScheduleApiService } from '../../services/api.schedule.service';
 
 @Component({
   selector: 'app-weekday-container',
@@ -8,12 +10,28 @@ import { WeekdaySelectorComponent } from './weekday-selector.component'; // Đ�
   templateUrl: './weekday-container.component.html',
   styleUrls: ['./weekday-container.component.css']
 })
-export class WeekdayContainerComponent {
-  weekdays = [
-    { name: 'MON', checked: true },
-    { name: 'TUE', checked: true },
+export class WeekdayContainerComponent implements OnInit {
+  private api = inject(ScheduleApiService);
+
+  weekdays: Weekday[] = [
+    { name: 'MON', checked: false },
+    { name: 'TUE', checked: false },
     { name: 'WED', checked: false },
-    { name: 'THU', checked: true },
+    { name: 'THU', checked: false },
     { name: 'FRI', checked: false },
   ];
+
+  ngOnInit(): void {
+    this.api.getCheckedDays().subscribe({
+      next: (daysFromApi) => this.updateCheckedState(daysFromApi),
+      error: (err) => console.error('Lỗi khi tải dữ liệu:', err)
+    });
+  }
+
+  private updateCheckedState(apiDays: string[]): void {
+    this.weekdays.forEach(day => {
+      day.checked = apiDays.includes(day.name);
+    });
+  }
+  
 }
