a2edf21e896e43c1cb1bc5c2c36d3019cde8ed83
vominhdat68 <datminhvo0901@gmail.com>
2026-02-04 10:52:23 +0700

    resolve merge conflicts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { SYSTEM_DESIGN_CONFIG } from 'dynamic-ds';
+import { REST_CONFIG, RestConfig } from '@goat-bravos/shared-lib-client';
 
 import { routes } from './app.routes';
 
@@ -11,27 +12,26 @@ export const appConfig: ApplicationConfig = {
     {
       provide: SYSTEM_DESIGN_CONFIG,
       useValue: {
-        brand: '#E18308', // Màu đại diện cho thương hiệu (từ mã bạn cung cấp)
-        primary: '#E18308', // Màu chính (từ mã bạn cung cấp)
-        secondary: '#505A5F', // Màu phụ (từ mã bạn cung cấp)
-        functional: '#E18308', // Màu chức năng (có thể dùng giống như primary)
-        utility: '#CF0026', // Màu tiện ích (thường dùng cho cảnh báo, lỗi)
-
-        // Các màu sắc khác
-        linearGradient: 'linear-gradient(to bottom, #FFFFFF80 16%, #FED8B34D 100%) ', // Gradient tuyến tính
-        radialGradient: 'radial-gradient(circle, #EDCEA7, #EDCEA700)', // Gradient đối xứng
-
-        // Màu cho các thành phần giao diện
-        text: '#E18308', // Màu chữ
-        button: '#E18308', // Màu nút bấm 1 (cam sáng)
-        buttonSecondary: '#505A5F', // Màu nút bấm 2 (xám tối)
-        imageBackground: '#FFF6E8', // Màu nền cho hình ảnh
-        panelBackground: '#FFF0DB', // Màu nền của panel chứa nút bấm
-        buttonBackground: '#F5A84A', // Màu nền cho nút bấm chính
-
-        // Màu cho shadow button
-        buttonShadow: '#FFC880A8', // Màu shadow cho nút bấm
+        brand: "#EE8E1B", // --brand-600
+        primary: "#E18308", // --primary-600
+        secondary: "#00652A", // --secondary-600
+        functional: "#C36F06", // --functional-600
+        utility: "#F5A84A", // --utility-600
       }
+    },
+
+    // Cấu hình REST API
+    {
+      provide: REST_CONFIG,
+      useValue: {
+        apiBaseUrl: 'http://localhost:4300/', // Base URL cho các hàm Internal
+        enableLogging: true,
+        internalAutoRetry: true,
+        retryAttempts: 3,
+        retryIntervalMs: 1000,
+        loginPath: '/login',
+        tokenKey: 'accessToken'
+      } as RestConfig
     }
   ]
 };
