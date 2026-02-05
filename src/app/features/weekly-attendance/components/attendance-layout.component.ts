d25ab89299090441e94d22d0f6029765ceda06f4
vominhdat68 <datminhvo0901@gmail.com>
2026-02-05 11:57:37 +0700

    style(attendance): update layout

--- src/app/features/weekly-attendance/components/attendance-layout.component.ts
+++ src/app/features/weekly-attendance/components/attendance-layout.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { AttendanceHeaderComponent } from '../components/attendance-header/attendance-header.component';
 import { WeekdayContainerComponent } from './weekday-selector/weekday-container.component';
 import { AttendanceContainerComponent } from '../components/attendance-container/attendance-container.component';
-import { AttendanceHistoryButtonComponent } from './button-action/attendance-history.component';
+import { AttendanceHistoryButtonComponent } from './button-action/attendance-history-button.component';
+import { TicketActionButtonComponent } from './button-action/create-ticket-button.component';
+import { MyTicketButtonComponent } from './button-action/my-tickets-button.component';
 
 @Component({
     selector: 'app-attendance-layout',
@@ -11,7 +13,9 @@ import { AttendanceHistoryButtonComponent } from './button-action/attendance-his
         AttendanceHeaderComponent,
         WeekdayContainerComponent,
         AttendanceContainerComponent,
-        AttendanceHistoryButtonComponent
+        AttendanceHistoryButtonComponent,
+        TicketActionButtonComponent,
+        MyTicketButtonComponent
     ],
     templateUrl: './attendance-layout.component.html',
     styleUrls: ['./attendance-layout.component.scss']
