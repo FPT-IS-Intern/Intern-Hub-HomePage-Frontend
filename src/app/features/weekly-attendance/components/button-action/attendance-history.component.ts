a2df039624d75bc466c5edfbb04d8a10669e9a40
vominhdat68 <datminhvo0901@gmail.com>
2026-02-05 09:48:10 +0700

    feat: add attendance history button component

---
+++ src/app/features/weekly-attendance/components/button-action/attendance-history.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { ButtonContainerComponent } from '@goat-bravos/intern-hub-layout';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-attendance-history-btn',
+  standalone: true,
+  imports: [ButtonContainerComponent],
+  templateUrl: './attendance-history.component.html',
+  styleUrls: ['./attendance-history.component.scss'],
+})
+export class AttendanceHistoryButtonComponent {
+  private router = inject(Router);
+
+  goToPage() {
+    this.router.navigate(['/dashboard']);
+  }
+}
