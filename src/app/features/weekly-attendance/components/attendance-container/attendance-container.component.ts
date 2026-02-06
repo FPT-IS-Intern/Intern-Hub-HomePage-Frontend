66e77f2f2afd400ce06f6d5680430f02f16f8fa5
vominhdat68 <datminhvo0901@gmail.com>
2026-02-06 16:13:38 +0700

    feat(attendance): integrate modal popup into attendance flow

--- src/app/features/weekly-attendance/components/attendance-container/attendance-container.component.ts
+++ src/app/features/weekly-attendance/components/attendance-container/attendance-container.component.ts
@@ -3,11 +3,12 @@ import { AttendanceService } from '../../services/api.attendance.service';
 import { AttendanceResponseData } from '../../models/attendance.model';
 import { AttendanceItemComponent } from './attendance-item.component'
 import { CommonModule } from '@angular/common';
+import { ModalComponent } from '../../../../libs/model-popup/modal.component';
 
 @Component({
   selector: 'app-attendance-container',
   standalone: true,
-  imports: [AttendanceItemComponent, CommonModule],
+  imports: [AttendanceItemComponent, CommonModule, ModalComponent],
   templateUrl: './attendance-container.component.html',
   styleUrls: ['./attendance-container.component.scss']
 })
@@ -27,19 +28,20 @@ export class AttendanceContainerComponent {
    * Logic xử lý chung cho cả Checkin và Checkout
    * @param action 'IN' | 'OUT'
    */
-  processAttendance(action: 'IN' | 'OUT') {
+  processAttendance(action: 'IN' | 'OUT', modal?: ModalComponent) {
     this.isLoading.set(true);
+    this.openConfirmPopupRemote(modal);
     this.service.checkNetwork().subscribe({
       next: (wifi) => {
         if (!wifi.isCompanyWifi) {
           this.isLoading.set(false);
           this.showPopup.set(true);
+
           return;
         }
 
-        // 2. Trong mạng -> Gọi API tương ứng
         const apiCall = action === 'IN' ? this.service.postCheckIn() : this.service.postCheckOut();
-    
+
         apiCall.subscribe({
           next: (res) => {
             this.isLoading.set(false);
@@ -48,7 +50,7 @@ export class AttendanceContainerComponent {
             } else {
               this.checkOut.set({ time: res.data.time, displayMessage: res.data.displayMessage, attendanceStatus: res.data.attendanceStatus });
             }
-                console.log('Calling API for action:', res);
+            console.log('Calling API for action:', res);
           },
           error: () => this.isLoading.set(false)
         });
@@ -61,14 +63,23 @@ export class AttendanceContainerComponent {
     this.showPopup.set(false);
     this.remoteRequestPending.set(true);
 
-    // Giả lập sau khi gửi phiếu Remote thành công
     setTimeout(() => {
-      this.remoteRequestPending.set(false);
       this.checkIn.set({
         time: '08:00',
         displayMessage: 'Đã gửi phiếu Remote - Chờ duyệt',
         attendanceStatus: 'SUCCESS'
       });
-    }, 2000);
+    }, 8000);
+  }
+
+  openConfirmPopupRemote(modal?: ModalComponent) {
+    modal?.open({
+      message: 'Hệ thống đang ghi nhận vị trí của bạn sai (hoặc ngoài bán kính cho phép) hoặc chưa có phiếu làm Remote. Vui lòng tạo phiếu',
+      cancelText: 'Hủy',
+      confirmText: 'Tạo phiếu',
+      panelClass: 'my-custom-modal',
+      onConfirm: () => this.createRemoteRequest(),
+      onCancel: () => this.showPopup.set(false),
+    });
   }
 }
