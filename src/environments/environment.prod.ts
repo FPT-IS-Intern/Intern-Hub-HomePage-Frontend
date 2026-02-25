df1bb45893be7b4de9943a4bbb4be19dff4b8819
vominhdat68 <datminhvo0901@gmail.com>
2026-02-26 02:09:47 +0700

    chore(config): update angular workspace and federation tsconfig

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    services: {
+        hrm: 'https://internhub-v2.bbtech.io.vn/api/hrm',
+        banner: 'https://api.production.com/banner-service',
+        auth: 'https://api.auth-server.com/api/v1',
+        scheduler: 'https://api.scheduler.com/api/v1',
+    }
+};
