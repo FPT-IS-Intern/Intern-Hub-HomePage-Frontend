03e8b76211fd3da1b09d9b0b39f3ba783dd425b1
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-03-01 16:42:09 +0700

    feat: Add Angular common and ng-zorro-antd to shared dependencies and enable ignoreUnusedDeps feature.

--- federation.config.js
+++ federation.config.js
@@ -2,30 +2,45 @@ const { withNativeFederation, shareAll } = require('@angular-architects/native-f
 
 module.exports = withNativeFederation({
   name: 'homePage',
-  
+
   exposes: {
     './routes': './src/app/app.routes.ts',
   },
-  
+
   shared: {
-    ...shareAll({ 
-      singleton: true, 
-      strictVersion: true, 
-      requiredVersion: 'auto' 
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
     }),
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+      includeSecondaries: true,
+    },
+    '@angular/common/locales/vi': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
     // QUAN TRỌNG: Phải share dynamic-ds với cùng config
     'dynamic-ds': {
       singleton: true,
       strictVersion: false,
       requiredVersion: 'auto',
-      includeSecondaries: true
-    }
+      includeSecondaries: true,
+    },
+    'ng-zorro-antd': {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: 'auto',
+      includeSecondaries: true,
+    },
   },
-  
-  skip: [
-    'rxjs/ajax',
-    'rxjs/fetch',
-    'rxjs/testing',
-    'rxjs/webSocket',
-  ],
+
+  skip: ['rxjs/ajax', 'rxjs/fetch', 'rxjs/testing', 'rxjs/webSocket'],
+  features: {
+    ignoreUnusedDeps: true,
+  },
 });
