b11833155eab7c66e476c619e1336034c8111ece
vominhdat68 <datminhvo0901@gmail.com>
2026-03-11 18:12:06 +0700

    update '/api'

--- src/app/core/config/app-config.ts
+++ src/app/core/config/app-config.ts
@@ -1,9 +1,9 @@
 export function getBaseUrl(serviceName?: string): string {
   const shellEnv = (window as any).__env;
 
-  // if (shellEnv && shellEnv.apiUrl) {
-  //   return shellEnv.apiUrl;
-  // }
+  if (shellEnv && shellEnv.apiUrl) {
+    return shellEnv.apiUrl;
+  }
 
   console.error('Shell environment not found! Application must be run within the Shell App.');
   return '';
