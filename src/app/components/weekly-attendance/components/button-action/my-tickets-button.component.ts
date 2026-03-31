67b3675c7e3bd18463ad0aa490c00b80939253f0
GWUY <buigiahuy1506@gmail.com>
2026-03-31 09:30:40 +0700

    add url to  navigate user ticket page

--- src/app/components/weekly-attendance/components/button-action/my-tickets-button.component.ts
+++ src/app/components/weekly-attendance/components/button-action/my-tickets-button.component.ts
@@ -3,16 +3,16 @@ import { ButtonContainerComponent } from '@goat-bravos/intern-hub-layout';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-my-tickets-btn',
-    standalone: true,
-    imports: [ButtonContainerComponent],
-    templateUrl: './my-tickets-button.component.html',
-    styleUrls: ['./button.component.scss']
+  selector: 'app-my-tickets-btn',
+  standalone: true,
+  imports: [ButtonContainerComponent],
+  templateUrl: './my-tickets-button.component.html',
+  styleUrls: ['./button.component.scss'],
 })
 export class MyTicketButtonComponent {
-    private router = inject(Router);
+  private router = inject(Router);
 
-    goToPage() {
-        this.router.navigate(['/dashboard']);
-    }
+  goToPage() {
+    this.router.navigate(['/ticket/my-tickets']);
+  }
 }
